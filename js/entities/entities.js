f4be6daacbd8ecfc0fcea08f8e8a09f56958f9c2
Josue Peralta-Soriano <peralta.josue09@gmail.com>
2015-01-22 11:06:33 -0800

    added comments for video 6

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -1,13 +1,13 @@
 game.PlayerEntity = me.Entity.extend({
-    init: function(x, y, settings) {
-        this._super(me.Entity, 'init', [x, y, {
-                image: "player",
-                width: 64,
+    init: function(x, y, settings) {//we set up our functions here
+        this._super(me.Entity, 'init', [x, y, {//this reaches into the constructer of entity's
+                image: "player",//this includes and image and its name
+                width: 64,//the width & height tell the screen what amount of space to preserve
                 height: 64,
-                spritewidth: "64",
+                spritewidth: "64",//sprite width and height pass the information
                 spriteheight: "64",
                 getShape: function() {
-                    return(new me.Rect(0, 0, 64, 64)).toPolygon();
+                    return(new me.Rect(0, 0, 64, 64)).toPolygon();//this returns a new shape
                 }
         }]);
     },
