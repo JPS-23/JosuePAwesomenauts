c6ec61ecc06daad2261cbb0b04e0696f869ca60b
Josue Peralta-Soriano <peralta.josue09@gmail.com>
2015-01-30 11:14:48 -0800

    finished video 13; Making my character attack + comments

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -16,7 +16,8 @@ game.PlayerEntity = me.Entity.extend({
     
         this.renderable.addAnimation("idle", [78]);
         this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 122, 123,124, 125], 80);
-    
+        this.renderable.addAnimation("attack", [65, 66, 67, 68, 69, 70, 71, 72], 80);
+        
         this.renderable.setCurrentAnimation("idle");
     },
     
@@ -31,13 +32,40 @@ game.PlayerEntity = me.Entity.extend({
             this.body.vel.x = 0;
         }
         
-        if(this.body.vel.x !== 0){
+        
+        
+        if(me.input.isKeyPressed("attack")) {//this is happening at the same time as the if statement
+            if(!this.renderable.isCurrentAnimation("attack")){
+                console.log(!this.renderable.isCurrentAnimation("attack"));
+                //Sets the current animation to attack and once that is over
+                //goes back to idle animation
+                this.renderable.setCurrentAnimation("attack", "idle");
+                //Make sit so that the next time we start this sequence we begin
+                //from the first animation, not wherever we left off when we
+                //switched to another animation 
+                this.renderable.setAnimationFrame();
+            }
+        }
+        else if(this.body.vel.x !== 0){
             if(!this.renderable.isCurrentAnimation("walk")){
                 this.renderable.setCurrentAnimation("walk");
         }
     }else{
         this.renderable.setCurrentAnimation("idle");
     }
+    
+    if(me.input.isKeyPressed("attack")) {//this is happening at the same time as the if statement
+            if(!this.renderable.isCurrentAnimation("attack")){
+                console.log(!this.renderable.isCurrentAnimation("attack"));
+                //Sets the current animation to attack and once that is over
+                //goes back to idle animation
+                this.renderable.setCurrentAnimation("attack", "idle");
+                //Make sit so that the next time we start this sequence we begin
+                //from the first animation, not wherever we left off when we
+                //switched to another animation 
+                this.renderable.setAnimationFrame();
+            }
+        }
         
         
         this.body.update(delta);//delta is the change of time that has happened
