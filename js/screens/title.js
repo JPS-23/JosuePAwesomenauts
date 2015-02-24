7eeb5c70e0b11fad37eca22e84b850241e7c7972
Josue Peralta-Soriano <peralta.josue09@gmail.com>
2015-02-24 11:23:58 -0800

    Finished video 27; Making the Title Screen Functional + Comments

--- js/screens/title.js
+++ js/screens/title.js
@@ -4,13 +4,35 @@ game.TitleScreen = me.ScreenObject.extend({
 	 */
 	onResetEvent: function() {//the code below is to load the title image	
 		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('title-screen')), -10); // TODO
-	},
+                //the line below adds a key to send us to our play screen
+                me.input.bindKey(me.input.KEY.ENTER, "start");
+                //the code below adds text to the game
+                me.game.world.addChild(new (me.Renderable.extend({
+                    init: function(){//this is a generic function/the inilization of the text
+                        this._super(me.Renderable, 'init', [510, 30, me.game.viewport.width, me.game.viewport.height]);
+                        this.font = new me.Font("Arial", 46, "white");//this gives us a font to use
+                    },
+                    
+                    draw: function(renderer){//this adds text on the screen
+                        this.font.draw(renderer.getContext(), "Awesomenauts!", 450, 130);
+                        this.font.draw(renderer.getContext(), "Press ENTER to play!", 250, 530);
+                    }
+                })));
+                //this is an event handler that gives the command to go from the title screen to the play screen
+                this.handler = me.event.subscribe(me.event.KEYDOWN, function (action, keyCode, edge){
+                   if(action === "start"){
+                       me.state.change(me.state.PLAY);
+                   } 
+                });
+        
+        },
 	
 	
 	/**	
 	 *  action to perform when leaving this screen (state change)
 	 */
-	onDestroyEvent: function() {
-		; // TODO
-	}
+	onDestroyEvent: function() {//this unbinds the ENTER KEY so that it doesnt make start all over during the game
+		me.input.unbindKey(me.input.KEY.ENTER); // TODO
+                me.event.unsubscribe(this.handler);
+        }
 });
