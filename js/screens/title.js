e01f08f29b2675dd7b6d0d4b26b28b6ecb98777c
Josue Peralta-Soriano <peralta.josue09@gmail.com>
2015-03-06 11:13:34 -0800

    Finished video 39; Start New + comments

--- js/screens/title.js
+++ js/screens/title.js
@@ -4,26 +4,33 @@ game.TitleScreen = me.ScreenObject.extend({
 	 */
 	onResetEvent: function() {//the code below is to load the title image	
 		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('title-screen')), -10); // TODO
-                //the line below adds a key to send us to our play screen
-                me.input.bindKey(me.input.KEY.ENTER, "start");
+                
                 //the code below adds text to the game
                 me.game.world.addChild(new (me.Renderable.extend({
                     init: function(){//this is a generic function/the inilization of the text
-                        this._super(me.Renderable, 'init', [510, 30, me.game.viewport.width, me.game.viewport.height]);
+                        this._super(me.Renderable, 'init', [270, 240, 300, 50]);
                         this.font = new me.Font("Arial", 46, "white");//this gives us a font to use
-                    },
+                        me.input.registerPointerEvent('pointerdown', this, this.newGame.bind(this), true);
+                    },//the line above lets us use our pointer to start the game
                     
                     draw: function(renderer){//this adds text on the screen
-                        this.font.draw(renderer.getContext(), "Awesomenauts!", 450, 130);
-                        this.font.draw(renderer.getContext(), "Press ENTER to play!", 250, 530);
+                        this.font.draw(renderer.getContext(), "START A NEW GAME", this.pos.x, this.pos.y);
+                    },
+                    
+                    update: function(dt){
+                        return true;
+                    },
+                    
+                    newGame: function(){
+                        me.input.releasePointerEvent('pointerdown', this);
+                        me.save.remove('exp');//here we are
+                        me.save.remove('exp1');//removing
+                        me.save.remove('exp2');//the 
+                        me.save.remove('exp3');//five
+                        me.save.remove('exp4');//variables
+                        me.state.change(me.state.PLAY);//this states the changes we just made
                     }
                 })));
-                //this is an event handler that gives the command to go from the title screen to the play screen
-                this.handler = me.event.subscribe(me.event.KEYDOWN, function (action, keyCode, edge){
-                   if(action === "start"){
-                       me.state.change(me.state.PLAY);
-                   } 
-                });
         
         },
 	
@@ -32,7 +39,6 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform when leaving this screen (state change)
 	 */
 	onDestroyEvent: function() {//this unbinds the ENTER KEY so that it doesnt make start all over during the game
-		me.input.unbindKey(me.input.KEY.ENTER); // TODO
-                me.event.unsubscribe(this.handler);
+
         }
 });
