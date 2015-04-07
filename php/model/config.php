cb1b1c2e037623d1fc6ce9bb3c0ea3351a47465b
Josue Peralta-Soriano <peralta.josue09@gmail.com>
2015-04-06 17:55:25 -0700

    Finished video 54; Starting PHP + comments

---
+++ php/model/config.php
@@ -0,0 +1,18 @@
+<?php
+    require_once(__DIR__ . "Database.php");
+    session_start();
+    //prevents hackers from hacking into a session without loggin in
+    session_regenerate_id(true);
+    
+    $path = '/JosuePAwesomenauts/php/';//this sets up a new destination
+    
+    $host = "localhost";
+    $username = "root";
+    $password = "root";
+    $database = "awesomenauts_db";//this is the name of the project
+    
+    if(!isset($_SESSION["connection"])){
+        echo "session";
+        $connection = new Database($host, $username, $password, $database);
+        $_SESSION["connection"] = $connection;
+    }
